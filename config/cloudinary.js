464d496b02d3e53810793b257d0558f97c282868
MilttO0nMss <grountish@gmail.com>
2020-05-22 16:23:37 +0200

    before mergin brings url again

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,6 @@
 const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
-
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_KEY,
@@ -11,7 +10,8 @@ cloudinary.config({
 var storage = cloudinaryStorage({
     cloudinary: cloudinary,
     folder: 'folder-name', // The name of the folder in cloudinary
-    allowedFormats: ['wav', 'vid', 'png', 'jpg', 'jpeg'],
+    allowedFormats: ['wav', 'png', 'jpg', 'jpeg'],
+    params: { resource_type: 'video' },//  =>this is in case you want to upload other type of files, not just images
     filename: function (req, file, cb) {
         cb(null, file.originalname); // The file on cloudinary would have the same  name as the original file name
     }
--- routes/scenesRouter.js
+++ routes/scenesRouter.js
@@ -16,7 +16,7 @@ router.get('/', (req, res, next) =>{
 })
 
 // POSTS A NEW SONG
-router.post('/', uploadCloud.single('file'), async (req, res, next) =>{
+router.post('/', async (req, res, next) =>{
     let {name, description, urlPath} = req.body
     let user = req.session.currentUser_id
     try{
@@ -28,6 +28,27 @@ router.post('/', uploadCloud.single('file'), async (req, res, next) =>{
         next(createError(err))
     }
 })
+
+router.post('/file', uploadCloud.single('urlPath'), async (req, res, next) =>{
+    if (!req.file) {
+        next(new Error('No file uploaded!'));
+        return;
+      }
+    //   // get secure_url from the file object and save it in the 
+    //   // variable 'secure_url', but this can be any name, just make sure you remember to use the same in frontend
+    //   res.json({ secure_url: req.file.secure_url });
+      console.log(req.file);
+    try{
+   res
+   .status(200)
+   .json({
+      urlPath: req.file.secure_url
+    })
+    } catch(err) {
+        next(createError(err))
+    }
+})
+
 
 router.delete('/:id', async (req, res, next) =>{
     const id = req.params.id;
